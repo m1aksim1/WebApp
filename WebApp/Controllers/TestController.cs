164501b15f2256172e28af64915a098f3440ce61
m1aksim1 <kosheevmaksim228@gmail.com>
2024-01-12 00:17:16 +0400

    кринж запостил

---
+++ WebApp/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareInstallationClientApp;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult CreateTest()
+        {
+            string userAgent = HttpContext.Request.Headers.UserAgent;
+            return View(APIClient.GetRequest<List<TheoryViewModel>>($"http://localhost:9002/theories/?user_agent={userAgent}" + $"&get_content={true}"));
+        }
+    }
+}
