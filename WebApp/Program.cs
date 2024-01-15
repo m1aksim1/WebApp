33ca400bfe6a1b0e966fadc2f982df98d9740dba
m1aksim1 <kosheevmaksim228@gmail.com>
2024-01-15 23:40:58 +0400

    кринж запостил

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -13,7 +13,6 @@ APIClient.Connect(builder.Configuration);
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
